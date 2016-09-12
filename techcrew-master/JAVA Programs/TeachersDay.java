ded8216a0e1f9e4b27ab3e33ce89485d780109ae
Salman khan <salmanaleemkhan786@gmail.com>
2016-09-12 20:42:34 +0530

    java programs

---
+++ techcrew-master/JAVA Programs/TeachersDay.java
@@ -0,0 +1,60 @@
+//package com.ise.teachersday;
+
+import java.util.Random;
+
+ class Mainclass {
+
+	public static void main(String[] args) {
+		int value;
+		Random rand=new Random();
+		value=rand.nextInt(15);
+		 switch (value) 
+		 {
+		 case 1:System.out.println("Number :1 \nDilshad mam");
+			
+			break;
+		case 2:System.out.println("Number :2 \nDeepak Sir");
+		
+		break;
+		case 3:System.out.println("Number :3 \nKhurram Sir");
+		
+		break;
+		case 4:System.out.println("Number :4 \nIshaq Sir");
+		
+		break;
+		case 5:System.out.println("Number :5 \nIliyaz Sir");
+		
+		break;
+		case 6:System.out.println("Number :6 \nVenktesh Sir");
+		
+		break;
+		case 7:System.out.println("Number :7 \nRaghavendra Sir");
+		
+		break;
+		case 8:System.out.println("Number :8 \nZabiha mam");
+		
+		break;
+		case 9:System.out.println("Number :9 \nZahra Amreen MAm");
+		
+		break;
+		case 10:System.out.println("Number :10 \nSalma mam");
+		
+		break;
+		case 11:System.out.println("Number :11 \nGANESHAN  Sir");
+		
+		break;
+		case 12:System.out.println("Number :12 \nSameena Mam");
+		
+		break;
+		case 13:System.out.println("Number :13 \nRajeshwari Mam");
+		
+		break;
+
+		
+		}
+			
+		
+		
+	}
+
+}
